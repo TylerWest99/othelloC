337ccd5c502b0b7f2d693a76f04f2dd246d57dea
Tyler West <tyler@TylerPC.localdomain>
2021-02-24 19:06:30 -0500

    fixes valid move and is move available funcs

--- othello.c
+++ othello.c
@@ -63,6 +63,11 @@ bool isValidMove(int size, char board[][size], int row, int col, char disc)
 {	
 	char notDisc;
 
+	int maxRow = size;
+	int maxCol = size;
+	int minRow = 1;
+	int minCol = 1;
+
 	if(disc == 'W'){
 		notDisc = 'B';
     }
@@ -70,105 +75,203 @@ bool isValidMove(int size, char board[][size], int row, int col, char disc)
 		notDisc = 'W';
     }
 
-	int maxRow = size;
-	int maxCol = size;
-	int minRow = 1;
-	int minCol = 1;
+	bool surrondedByOpposite = ((board[row+1][col] == notDisc && row+1 <= maxRow) || (board[row-1][col] == notDisc && row-1 >= minRow) || (board[row][col+1] == notDisc &&  col+1 <= maxCol) ||
+								(board[row][col-1] == notDisc && col-1 >= minCol) || (board[row-1][col-1] == notDisc && col-1 >= minCol && row-1 >= minRow) || 
+								(board[row+1][col+1] == notDisc && col+1 >= minCol && row+1 >= minRow) || (board[row+1][col-1] == notDisc && col-1 >= minCol && row+1 <= maxRow) ||
+								(board[row-1][col+1] == notDisc && col+1 <= maxCol && row-1 >= minRow));
 
+	bool tlOpp = (board[row-1][col-1] == notDisc && col-1 >= minCol && row-1 >= minRow);
+	bool trOpp = (board[row-1][col+1] == notDisc && col+1 <= maxCol && row-1 >= minRow);
+	bool tmOpp = (board[row-1][col] == notDisc && row-1 >= minRow);
+	bool mrOpp = (board[row][col+1] == notDisc &&  col+1 <= maxCol);
+	bool mlOpp = (board[row][col-1] == notDisc && col-1 >= minCol);
+	bool blOpp = (board[row+1][col-1] == notDisc && col-1 >= minCol && row+1 <= maxRow);
+	bool bmOpp = (board[row+1][col] == notDisc && row+1 <= maxRow);
+	bool brOpp = (board[row+1][col+1] == notDisc && col+1 >= minCol && row+1 >= minRow);
 
-	int c = 1; //count var
+	bool bl = false;
+	bool bm = false;
+	bool br = false;
+	bool mr = false;
+	bool ml = false;
+	bool tr = false;
+	bool tm = false;
+	bool tl = false;
 
-	/* row - */
-	if(board[row][col] == '-' && board[row-1][col] == notDisc && row-1 >= minRow){ /* checks to see if spot is empty and spot above is notDisc */
-		while((row-c >= minRow) && board[row-c][col] == notDisc){ /* while in range of top and bottom rows */
-			if(board[row-c-1][col] == disc){ /* if the next one after is the same as disc return true */
-				return true;
-            }
+	int c = 1;
+
+	if(surrondedByOpposite){
+		//tl
+		if(tlOpp){
+			while(board[row-c][col-c] == notDisc && row-c >= minRow && col-c >= minCol){
+				c++;
+			}
+			if(board[row-c][col-c] == disc){
+				tl = true;
+			}
+		}
+		c = 1;
+		
+		//tr
+		if(trOpp){
+			while(board[row-c][col+c] == notDisc && row-c >= minRow && col+c <= maxCol){
+				c++;
+			}
+			if(board[row-c][col+c] == disc){
+				tr = true;
+			}
+		}
+		c = 1;
+
+		//tm
+		if(tmOpp){
+			while(board[row-c][col] == notDisc && row-c >= minRow){
+				c++;
+			}
+			if(board[row-c][col] == disc){
+				tm = true;
+			}
+		}
+		c = 1;
+
+		//ml
+		if(mlOpp){
+			while(board[row][col-c] == notDisc && col-c >= minCol){
+				c++;
+			}
+			if(board[row][col-c] == disc){
+				ml = true;
+			}
+		}
+		c = 1;
+
+		//mr
+		if(mrOpp){
+			while(board[row][col+c] == notDisc && col+c <= maxCol){
+				c++;
+			}
+			if(board[row][col+c] == disc){
+				mr = true;
+			}
+		}
+		c = 1;
+
+		//bl
+		if(blOpp){
+			while(board[row+c][col-c] == notDisc && row+c <= maxRow  && col-c >= minCol){
+				c++;
+			}
+			if(board[row+c][col-c] == disc){
+				bl = true;
+			}
+		}
+		c = 1;
+
+		
+		//bm
+		if(bmOpp){
+			while(board[row+c][col] == notDisc && row+c <= maxRow){
+				c++;
+			}
+			if(board[row+c][col] == disc){
+				bm = true;
+			}
+		}
+		c = 1;
+
+		//br
+		if(brOpp){
+			while(board[row+c][col+c] == notDisc && row+c <= maxRow  && col+c <= maxCol){
+				c++;
+			}
+			if(board[row+c][col+c] == disc){
+				br = true;
+			}
+		}
+		c = 1;
+	} 
+
+	if(tm){
+		while(board[row-c][col] == notDisc){
 			c++;
-        }
-    }
+		}
+		if(board[row-c][col] == disc){
+			return true;
+		}
+	}
 	c = 1;
 
-	/* row + */
-	if(board[row][col] == '-' && board[row+1][col] == notDisc && row+1 <= maxRow){ /* checks to see if spot is empty and spot above is notDisc */
-		while((row+c <= maxRow) && board[row+c][col] == notDisc){ /* while in range of top and bottom rows */
-			if(board[row+c+1][col] == disc){ /* if the next one after is the same as disc return true */
-				return true;
-            }
+	if(bm){
+		while(board[row+c][col] == notDisc){
 			c++;
-        }
-    }
+		}
+		if(board[row+c][col] == disc){
+			return true;
+		}
+	}
 	c = 1;
 
-	/* col - */
-	if(board[row][col] == '-' && board[row][col-1] == notDisc && col-1 >= minCol){ /* checks to see if spot is empty and spot above is notDisc */
-		while((col-c >= minCol) && board[row][col-c] == notDisc){ /* while in range of top and bottom rows */
-			if(board[row][col-c-1] == disc){
-				return true;
-            }
+	if(ml){
+		while(board[row][col-c] == notDisc){
 			c++;
-        }
-    }
+		}
+		if(board[row][col-c] == disc){
+			return true;
+		};
+	}
 	c = 1;
 
-	/* col + */
-	if(board[row][col] == '-' && board[row][col+1] == notDisc && col+1 <= maxCol){ /* checks to see if spot is empty and spot around is notDisc */
-		while((col+c <= maxCol) && board[row][col+c] == notDisc){ /* while in range of top and bottom rows */
-			if(board[row][col+c+1] == disc){
-				return true;
-            }
+	if(mr){
+		while(board[row][col+c] == notDisc){
 			c++;
-        }
-    }
+		}
+		if(board[row][col+c] == disc){
+			return true;
+		}
+	}
 	c = 1;
 
-	/* col + row + */
-	if(board[row][col] == '-' && board[row+1][col+1] == notDisc && col+1 <= maxCol && row+1 <=maxRow){ /* checks to see if spot is empty and spot around is notDisc */
-		while((col+c <= maxCol && row+c <= maxRow) && board[row+c][col+c] == notDisc){ /* while in range of top and bottom rows */
-			if(board[row+c+1][col+c+1] == disc){
-				return true;
-            }
+	if(tl){
+		while(board[row-c][col-c] == notDisc){
 			c++;
-        }
-    }
+		}
+		if(board[row-c][col-c] == disc){
+			return true;
+		};
+	}
 	c = 1;
 
-	/* col + row - */
-	if(board[row][col] == '-' && board[row-1][col+1] == notDisc && col+1 <= maxCol && row-1 >= minRow){ /* checks to see if spot is empty and spot around is notDisc */
-		while((col+c <= maxCol && row-c >= minRow) && board[row-c][col+c] == notDisc){ /* while in range of top and bottom rows */
-			if(board[row-c-1][col+c+1] == disc){
-				return true;
-            }
+	if(br){
+		while(board[row+c][col+c] == notDisc){
 			c++;
-        }
-    }
+		}
+		if(board[row+c][col+c] == disc){
+			return true;
+		}
+	}
 	c = 1;
 
-	/* col - row - */
-	if(board[row][col] == '-' && board[row-1][col-1] == notDisc && col-1 >= minCol && row-1 >= minRow){ /* checks to see if spot is empty and spot around is notDisc */
-		while((col-c >= minCol && row-c >= minRow) && board[row-c][col-c] == notDisc){ /* while in range of top and bottom rows */
-			if(board[row-c-1][col-c-1] == disc){
-				return true;
-            }
+	if(bl){
+		while(board[row+c][col-c] == notDisc){
 			c++;
-        }
-    }
+		}
+		if(board[row+c][col-c] == disc){
+			return true;
+		}
+	}
 	c = 1;
 
-	/* col - row + */
-	if(board[row][col] == '-' && board[row+1][col-1] == notDisc && col-1 >= minCol && row+1 <=maxRow){ /* checks to see if spot is empty and spot around is notDisc */
-		while((col-c >= minCol && row+c <= maxRow) && board[row+c][col-c] == notDisc){ /* while in range of top and bottom rows */
-			if(board[row+c+1][col-c-1] == disc){
-				return true;
-            }
+	if(tr){
+		while(board[row-c][col+c] == notDisc){
 			c++;
-        }
-    }
+		}
+		if(board[row-c][col+c] == disc){
+			return true;
+		};
+	}
 	c = 1;
-	
 
 	return false;
-	//DONE
 } 
 
 // Places the disc at location row,col and flips the opponent discs as needed ""
@@ -230,11 +333,13 @@ void placeDiscAt(int size, char board[][size], int row, int col, char disc){
         }
     }
 	if(board[row-1][col-1] == notDisc && row-1 >= minRow && col-1 >= minCol){ //tl  
-		for(int i = 1; i < size; i++){
-			if(board[row-i][col-i] == disc && row-i >= minRow && col-i >= minCol){
+		while((board[row-c][col-c] == notDisc || board[row-c][col-c] == disc) && col-c >= minCol && row-c >= minRow){
+			if(board[row-c][col-c] == disc){
 				tl = true;
-            }
-        }
+				printf("true");
+			}
+			c++;
+		}
     }
 	if(board[row+1][col-1] == notDisc && row && row+1 <= maxRow && col-1 >= minCol){ //bl
 		for(int i = 1; i < size; i++){
@@ -250,6 +355,15 @@ void placeDiscAt(int size, char board[][size], int row, int col, char disc){
             }
         }
     }
+	if(board[row-1][col] == notDisc && row-1 >= minRow){ /*tm */
+		for(int i = 1; i < size; i++){
+			if(board[row-i][col] == disc && row-i >= minRow){
+				tm = true;
+            }
+        }
+    }
+
+
 
 
 	if(isValidMove(size,board,row,col,disc)){
@@ -314,7 +428,7 @@ void placeDiscAt(int size, char board[][size], int row, int col, char disc){
 		board[row][col] = disc;
 	}
  	//return;	
-	// Done
+	// wonky still
 }
 
 /* Returns true if a valid move for disc is available; false otherwise */
@@ -322,7 +436,7 @@ bool isValidMoveAvailable(int size, char board[][size], char disc)
 {
 	for(int i = 0; i <= size; i++){
 		for(int j = 0; j <= size; j++){
-			if(isValidMove(size, board, i, j, disc)){
+			if(isValidMove(size, board, i, j, disc) && board[i][j] == '-'){
 				return true;
 			}
 		}
